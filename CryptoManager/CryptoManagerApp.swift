421873bf2a37007f40ae589c57bf589709c1989e
Aram <aramsaribekyan1@gmail.com>
2024-06-09 09:31:48 +0400

    Initial Commit

---
+++ CryptoManager/CryptoManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoManagerApp.swift
+//  CryptoManager
+//
+//  Created by Aram on 09.06.24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
